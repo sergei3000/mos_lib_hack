80761e19a617b79ab6be09c4cfe7b9edf31b2e45
sergei3000 <mrsergeycity@gmail.com>
2021-10-16 23:40:20 +0000

    Implement querying MySQL instead of mocks

--- services/recommendation_services.py
+++ services/recommendation_services.py
@@ -1,11 +1,21 @@
 from services import db_services
 
+RECOMMENDATIONS_TABLE = "recommendations"
+RECOMMENDATIONS_FIELDS = ["item_id", "title", "author", "ranking"]
+HISTORY_TABLE = "history"
+HISTORY_FIELDS = ["item_id", "title", "author"]
+USER_FIELD = "user_id"
+DEFAULT_USER_ID = 0
+
 
 async def get_recommendations(user_id: int, connection_pool):
-    sql_query_string = "SELECT item_id, title, author, rank FROM recommendations where user_id = (%s);"
-    sql_query_params = (user_id,)
-    query_output = await db_services.execute_query(
-        sql_query_string, sql_query_params, connection_pool, fetch="all"
+    query_output = await db_services.select_with_filter(
+        connection_pool,
+        RECOMMENDATIONS_TABLE,
+        RECOMMENDATIONS_FIELDS,
+        USER_FIELD,
+        user_id,
+        DEFAULT_USER_ID
     )
     result = [{"id": row[0], "title": row[1], "author": row[2], "rank": row[3]}
         for row in query_output]
@@ -14,10 +24,13 @@ async def get_recommendations(user_id: int, connection_pool):
 
 
 async def get_history(user_id: int, connection_pool):
-    sql_query_string = "SELECT item_id, title, author FROM history where user_id = (%s);"
-    sql_query_params = (user_id,)
-    query_output = await db_services.execute_query(
-        sql_query_string, sql_query_params, connection_pool, fetch="all"
+    query_output = await db_services.select_with_filter(
+        connection_pool,
+        HISTORY_TABLE,
+        HISTORY_FIELDS,
+        USER_FIELD,
+        user_id,
+        DEFAULT_USER_ID
     )
     result = [{"id": row[0], "title": row[1], "author": row[2]}
         for row in query_output]
