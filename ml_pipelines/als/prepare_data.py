f2ff9595e1c6909740d5b453ba360da836bc1290
Кисмат <kismatisaev@MacBook-Pro-Kismat.local>
2021-10-22 22:31:38 +0300

    add preparation of books_full

--- ml_pipelines/als/prepare_data.py
+++ ml_pipelines/als/prepare_data.py
@@ -1,6 +1,69 @@
 import datetime
+import json
+import numpy as np
 import pandas as pd
 from utils import load_mapping, save_mapping
+
+
+def prepare_cat_file():
+     cat = []
+     for i in range(1, 4):
+         cat.append(
+             pd.read_csv(f"../../data/raw/cat_{i}.csv", encoding="cp1251", delimiter=";")
+         )
+     cat = pd.concat(cat).reset_index(drop=True)
+     cat.to_parquet(
+         path="../../data/cat.parquet.gzip",
+         engine="fastparquet",
+         compression="gzip",
+         index=False,
+     )
+
+
+def prepare_books_full_file():
+    parsed_books_full = []
+    invalid_rows = []
+    with open('books_full.jsn.tag') as f:
+        for i, line in enumerate(f):
+            try:
+                data = json.loads(line[:-2])
+                parsed_books_full.append([
+                    data["id"], data["title"], data["title_orig"],
+                    data["author_fullName"], data["author_surname"], data["author_fullNameAlt"],
+                    data["rubric_name"],
+                    data["smart_collapse_field"],
+                ])
+            except:
+                invalid_rows.append(i)
+    books_full = pd.DataFrame(
+        parsed_books_full,
+        columns=[
+            "id", "title", "title_orig",
+            "author_fullName", "author_surname", "author_fullNameAlt",
+            "rubric_name", "smart_collapse_field"
+        ]
+    )
+
+    conditions = [
+        (books_full['title_orig'] != '') & (books_full['title_orig'].isna() == False),
+        (books_full['title'] != '') & (books_full['title'].isna() == False)
+    ]
+    choices = [books_full['title_orig'], books_full['title']]
+    books_full['title_full'] = np.select(conditions, choices, default='Без названия')
+
+    books_full = books_full[["id", "title_full", "author_fullName", "rubric_name"]]
+    books_full = books_full.rename(columns={"id": "recId", "title_full": "title", "author_fullName": "author"})
+
+    cat = pd.read_parquet("../../data/cat.parquet.gzip")
+    from_cat2books_full = cat[cat['recId'].isin(set(cat["recId"]) - set(books_full["recId"]))][["recId", "title", "aut", "rubrics"]]
+    from_cat2books_full = from_cat2books_full.rename(columns={"aut": "author", "rubrics": "rubric_name"})
+
+    books_full = pd.concat([books_full, from_cat2books_full]).reset_index(drop=True)
+    books_full["author"] = books_full["author"].fillna("неизвестный")
+    books_full["title"] = books_full["title"].fillna("неизвестно")
+    books_full.to_parquet(
+        path="../../data/books_full.parquet.gzip", engine="fastparquet", compression="gzip", index=False
+    )
 
 
 def prepare_mappings_for_recid():
